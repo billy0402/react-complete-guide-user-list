eaba999c6c2f7b1674447cef58f44315d21621d4
Billy Huang <10646003@ntub.edu.tw>
2022-01-31 22:16:52 +0800

    feat(AddUser): Managing the User Input State

--- src/components/user/AddUser.tsx
+++ src/components/user/AddUser.tsx
@@ -1,20 +1,34 @@
-import { SyntheticEvent } from 'react';
+import { SyntheticEvent, useState } from 'react';
 
 import Button from '@components/ui/Button';
 import Card from '@components/ui/Card';
 
 const AddUser = () => {
+  const [username, setUsername] = useState('');
+  const [age, setAge] = useState('');
+
   const addUserHandler = (event: SyntheticEvent) => {
     event.preventDefault();
+    console.log(username, age);
+  };
+
+  const usernameChangeHandler = (
+    event: React.ChangeEvent<HTMLInputElement>,
+  ) => {
+    setUsername(event.target.value);
+  };
+
+  const ageChangeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setAge(event.target.value);
   };
 
   return (
     <Card className='input'>
       <form onSubmit={addUserHandler}>
         <label htmlFor='username'>Username</label>
-        <input id='username' type='text' />
+        <input id='username' type='text' onChange={usernameChangeHandler} />
         <label htmlFor='age'>Age (Years)</label>
-        <input id='age' type='number' />
+        <input id='age' type='number' onChange={ageChangeHandler} />
         <Button type='submit'>Add User</Button>
       </form>
     </Card>
